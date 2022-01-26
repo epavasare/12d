09fa6fddda7e50dc0b9d8b0e7665e4cbba5e7d35
Elize Pavasare <98461079+epavasare@users.noreply.github.com>
2022-01-26 12:28:21 +0000

    papild uzdevums

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
+#import math
+#a=int(input("Ievadi kvadrāta malas garumu lielāku par 5cm :"))
+#laukums=math.pow(a,2)
+#b=a+5
+#laukums2=math.pow(b,2)
+#c=(laukums2-laukums)/laukums*100
+#print(round(c),"%")
 import math
-a=int(input("Ievadi kvadrāta malas garumu lielāku par 5cm :"))
-laukums=math.pow(a,2)
-b=a+5
-laukums2=math.pow(b,2)
-c=(laukums2-laukums)/laukums*100
-print(round(c),"%")
+a=int(input("Ievadi riņķa rādiusu:"))
+b=int(input("Ievadi vienādsāna taisnleņķa trijstūra hipotenūzas garumu : "))
+rinkaS=math.pi*math.pow(a,2)
+trijsturaS=b*(b/2)/2
+lielaks=rinkaS-trijsturaS
+print("Riņķa laukums ir par",round(lielaks, 1),"cm^3 lielāks nekā trijstūra laukums.")
